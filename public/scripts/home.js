4517e657aaae5e2d7e40766a37ad185a82c28b29
MrGreque <gabrielgreque9910@gmail.com>
2021-10-07 21:42:04 -0300

    Added videos with object in the first page

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -19,24 +19,32 @@ function viewDivLogin(){
 }
 
 const videos = [
-  {message: 'teste', image: '..\\img\\pubg.jpg'},
-  {message: 'teste2', image: '..\\img\\pubg.jpg'}
+  {message: 'Video 1', image: '..\\img\\ep1.jpg', href: 'https://www.youtube.com/channel/UCKcl2rgbCqK9_njUVfTcUIA/featured' , views: 300},
+  {message: 'Video 2', image: '..\\img\\ep2.jpg', href: 'https://www.youtube.com/channel/UCKcl2rgbCqK9_njUVfTcUIA/featured' , views: 150},
+  {message: 'Video 3', image: '..\\img\\ep3.png', href: 'https://www.youtube.com/channel/UCKcl2rgbCqK9_njUVfTcUIA/featured' , views: 1254},
+  {message: 'Video 4', image: '..\\img\\ep4.png', href: 'https://www.youtube.com/channel/UCKcl2rgbCqK9_njUVfTcUIA/featured' , views: 3008}
 ]
 
 function createVideos(videos){
 
-    videos.forEach( ({message , image}) => {
+    videos.forEach( ({message , image, href, views}) => {
 
         let classVideos = document.getElementById('videos');
         let messageVideo = document.createElement('p');
         messageVideo.innerHTML = message;
+        //messageVideo.className
+
+        let youtube = document.createElement('a');
+        youtube.href = href;
 
         let imageVideo = new Image()
         imageVideo.src = image;
 
+        youtube.appendChild(imageVideo);
+
         let div = document.createElement('div');
         div.appendChild(messageVideo);
-        div.appendChild(imageVideo);
+        div.appendChild(youtube);
 
         classVideos.appendChild(div);
     });
